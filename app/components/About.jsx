928f44de0ec3364004e7c8ddaa71aa5b8e58216e
Liu <jliu5@HFHS.ORG>
2017-09-07 15:26:31 -0400

    Style About page and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,18 +1,20 @@
 var React = require('react');
 
-/*var About = React.createClass({
-  render: function () {
-    return (
-      <h3>About Component</h3>
-    )
-  }
-});*/
 
 var About = (props) => {
 	return (
 	<div>
-      <h3>About</h3>
-      <p>Welcome to About page</p>
+      <h1 className ="text-center">About</h1>
+      <p>This is a weather application build on React. This is complete React Web App</p>
+      <p>Here are some tools we users:</p>
+      <ul>
+      	<li>
+      		<a href="https://facebook.github.io/react">React</a> - This was the JavaScropt framework used.
+      	</li>
+      	<li>
+      		<a href="http://openweathermap.org">Open Weather Map</a> - used this link to search.
+      	</li>
+      </ul>
      </div>
     )
 }
